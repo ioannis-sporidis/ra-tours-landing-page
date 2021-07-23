e8f129187e37f96cb07f2739e8a3d490ee7a6934
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-23 10:40:34 -0400

    Setup fetch api data and loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { useState, useEffect } from 'react';
+import Loading from './UI/Loading';
+import Tours from './components/Tours';
+
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <div className='App'></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const fetchedTours = await response.json();
+      setIsLoading(false);
+      setTours(fetchedTours);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
